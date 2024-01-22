1a8a21f4eb0e31b559212d40cb49a8adc736e207
Martí Serra Vivancos <martiserravivancos@gmail.com>
2024-01-22 13:32:21 +0100

    chore: update smtp settings

--- mailer.rb
+++ mailer.rb
@@ -6,8 +6,6 @@ Rails.application.config.action_mailer.smtp_settings = {
   :address => 'smtp.resend.com',
   :user_name => 'resend',
   :password => ENV['RESEND_API_KEY'],
-  :domain => 'happyscribe.com',
   :port => 465,
-  :authentication => :plain,
   :tls => true,
 }
